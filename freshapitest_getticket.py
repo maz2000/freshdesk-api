fd8d7b382d016e371eaaa197fc9123602e4eda87
Masakazu Takahashi <masakaz@gmail.com>
2022-02-04 13:19:18 +0900

    Update freshapitest_getticket.py

--- freshapitest_getticket.py
+++ freshapitest_getticket.py
@@ -2,8 +2,8 @@ import requests
 import json
 from pprint import pprint 
 
-api_key = "7MlnYp82iwiT4bG4xO42"
-domain = "newaccount1641238466306"
+api_key = "API_KEY"
+domain = "DOMAIN"
 password = "x"
 
 url = requests.get("https://"+ domain +".freshdesk.com/api/v2/tickets?filter=new_and_my_open", auth = (api_key, password))
